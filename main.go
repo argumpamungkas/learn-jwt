7427af66eb5ed3431a9c0346ec0f921bfb4ab065
argumpamungkas <argumpamungkaass@gmail.com>
2023-04-08 10:59:50 +0700

    update

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"DTS/Chapter-3/sesi/sesi2-go-jwt/repo"
 	"DTS/Chapter-3/sesi/sesi2-go-jwt/router"
-	"os"
 )
 
 func main() {
 	repo.StartDB()
-
-	var PORT = os.Getenv("PORT")
-	router.StartApp().Run(":" + PORT)
+	
+	router.StartApp().Run(":8080")
 }
